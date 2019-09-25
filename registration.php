b2a31186ff85658d422b1fa4c1c1cfe62d80c4c0
glahoria <52441291+glahoria@users.noreply.github.com>
2019-09-25 11:14:55 +0530

    add php code

--- registration.php
+++ registration.php
@@ -1,3 +1,12 @@
+<?php
+echo "<pre>"; print_r($_GET) ; echo "</pre>";
+echo "<pre>"; print_r($_POST) ; echo "</pre>";
+echo "<pre>"; print_r($_REQUEST) ; echo "</pre>";
+ if(!empty($_POST['email'])){
+	// echo "<pre>"; print_r($_POST) ; echo "</pre>";
+	 die;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
