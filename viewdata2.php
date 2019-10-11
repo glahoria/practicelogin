5d0307e6d23d3137626fae98c832e69bff46b46e
root <parambsingh>
2019-10-11 08:13:22 -0600

    Amenity page, Images and 4step flow fixed

--- viewdata2.php
+++ viewdata2.php
@@ -1,44 +1,46 @@
 <?php include "connection.php"; ?>
 <?php
-	//parse_str($_SERVER['QUERY_STRING'], $queries); 
-	
-	$_GET['sort_order'] =  empty($_GET['sort_order']) ? "asc" : (($_GET['sort_order'] == "asc") ? "desc" : "asc");  
-    
 	$query = "SELECT * FROM `users`" ;
 	
-	if(!empty($_GET['key'])){
+	$key = "";
+	if(!empty($_REQUEST['key'])){
+			$key = $_REQUEST['key'];
 			$searchInFields = ['first_name','last_name','email','phone'];
 			$whereArray = [];
 			foreach($searchInFields as $searchInField){
-				$whereArray[] = $searchInField." LIKE '%".$_GET['key']."%'";
+				$whereArray[] = $searchInField." LIKE '%".$key."%'";
 				//$whereArray[] = $searchInFriled." = '".$_GET['key']."'";
 			}
 			
 			$query = $query. " WHERE " . implode(" OR ", $whereArray);
+			$_GET['key'] = $key;
 				
 	} 
+	
+	
+    $exe = mysqli_query($conn, $query );
+    $limit = 2;
+    $rows = mysqli_num_rows($exe);
+    $pages = ceil($rows/$limit);
+    $page = isset($_GET['page']) ?  $_GET['page'] : 1;
+    $offset = ($page-1)*$limit;
+    $Previous = $page-1 ;
+    $Next = $page+1 ;
+    $i = 1;
+	
+	$_GET['sort_order'] =  empty($_GET['sort_order']) ? "asc" : (($_GET['sort_order'] == "asc") ? "desc" : "asc");  
 	
 	if(!empty($_GET['sort_by'])){
 			$query = $query. " ORDER BY " . $_GET['sort_by']." " .$_GET['sort_order'];
 	} 
 	
-	//pr($query);
-	
 	unset($_GET['sort_by']);
 	
-	$url = $_SERVER['PHP_SELF']."?".http_build_query($_GET)."&sort_by=";
-    $insert = mysqli_query($conn, $query );
-    ///////////pagination////////////
-	$limit = 5;
-    $rows = mysqli_num_rows($insert);
-    $pages = ceil($rows/$limit);
-    $page = isset($_GET['page']) ?  $_GET['page'] : 1;
-    $offset = ($page-1)*$limit;
-    $query = "SELECT * FROM accounts LIMIT " .$offset.',' . $limit;
-    $insert = mysqli_query($conn,$query);
-    $Previous = $page-1 ;
-    $Next = $page+1 ;
-    $i = 1;
+	$query = $query. " LIMIT ".$offset.",".$limit;
+	
+	$exe = mysqli_query($conn, $query );
+	
+	$url = $_SERVER['PHP_SELF']."?".http_build_query($_GET)."&page=PAGE&sort_by=";
     
 ?>
 
@@ -55,6 +57,7 @@
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
     <style>
 		.sort th {cursor:pointer;}
+		.active a{color:red !important;}
     </style>
 </head>
 <body>
@@ -62,26 +65,26 @@
         <div class="col-md-12 m-auto">
             <h1 class="text-dark text-center">Display data</h1>	
             <div class="col-md-4 float-left">
-                <h3 class="text-primary font-weight-bold">Showing Page: <?php echo $_GET['page']; ?> / <?php echo $pages; ?></h3>
+                <h3 class="text-primary font-weight-bold">Showing Page: <?= $page; ?> / <?php echo $pages; ?></h3>
             </div>
             
             <div class="col-md-4 float-right my-3">
-            <form action="">
-				<input type="text" name="key" id="searchKey" class="form-control-sm form-control" placeholder="Search...">
+            <form action="viewdata2.php?page=<?= $page; ?>" method="post">
+				<input type="text" name="key" id="searchKey" class="form-control-sm form-control" value="<?= $key; ?>" placeholder="Search...">
             </form>
             </div>
             </div>
             <table class="table table-striped table-hover table-bordered">
                 <tr class="text-center bg-primary text-white sort">
                     <th>Sr No.</th>
-                    <th><a class="text-white" href="<?= $url; ?>first_name">First Name</a></th>
-                    <th><a class="text-white" href="<?= $url; ?>last_name">Last Name</a></th>
-                    <th><a class="text-white" href="<?= $url; ?>email">Email</a></th>
-                    <th><a class="text-white" href="<?= $url; ?>phone">Phone</a></th>
+                    <th><a class="text-white" href="<?= str_replace("PAGE", $page, $url); ?>first_name">First Name</a></th>
+                    <th><a class="text-white" href="<?= str_replace("PAGE", $page, $url); ?>last_name">Last Name</a></th>
+                    <th><a class="text-white" href="<?= str_replace("PAGE", $page, $url); ?>email">Email</a></th>
+                    <th><a class="text-white" href="<?= str_replace("PAGE", $page, $url); ?>phone">Phone</a></th>
                     <th>Action</th>
                 </tr>
 		 
-	        	<?php	while ($result = mysqli_fetch_array($insert)) { ?>
+	        	<?php	while ($result = mysqli_fetch_array($exe)) { ?>
     	        <tr class="text-center">
                     <td><?php echo $i; ?></td>
                     <td><?php echo $result['first_name']; ?></td>
@@ -102,15 +105,14 @@
     <div class="col-md-6 float-right">
     	<nav aria-label="Page navigation example">
     	    <ul class="pagination">
-    	    	<?php if ($page > 1) { 
-    	    		?>
-                <li class="page-item"><a class="page-link" href="viewdata2.php?page=<?= $Previous; ?>" aria-label="Previous">&laquo; Previous</a></li>
+    	    	<?php if ($page > 1) { ?>
+                <li class="page-item"><a class="page-link" href="<?= str_replace("PAGE", $Previous, $url); ?>" aria-label="Previous">&laquo; Previous</a></li>
                 <?php } ?>
                 <?php for($i = 1; $i <= $pages; $i++) : ?>
-                    <li class="active"><a  class="page-link" href="viewdata2.php?page=<?= $i; ?>"><?= $i; ?></a></li>     
+                    <li <?php if($page == $i) { ?>class="active" <?php } ?>><a  class="page-link" href="<?= str_replace("PAGE", $i	, $url); ?>"><?= $i; ?></a></li>     
 		        <?php endfor; ?>
 		        <?php if ($page != $pages) { ?>
-                <li class="page-item"><a class="page-link" href="viewdata2.php?page=<?= $Next; ?>" aria-label="Next">Next &raquo;</a></li>
+                <li class="page-item"><a class="page-link" href="<?= str_replace("PAGE", $Next, $url); ?>" aria-label="Next">Next &raquo;</a></li>
                 <?php } ?>
             </ul>
     </div>    
@@ -130,10 +132,8 @@ $(function() {
 			});
 			$(this).parents('tr').hide(); 
 		 }
-
 		return false;
 	});
-	
 });
 </script>
 </body>
